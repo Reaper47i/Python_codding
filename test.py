0b992e3ed136866611464c0d4cc3e8217e0f8987
Reaper47i <vineetbhengra96@gmail.com>
2022-01-25 01:24:36 +0530

    new commit

--- test.py
+++ test.py
@@ -1,13 +1,35 @@
-a = [28 ,7 ,-36 ,21 ,-21 ,-50 ,9 ,-32]
-a.sort()
-print(a)
-r = [0]*2
-k = 1
-_sum = 0
-for i in range(len(a)):
-    _sum += a[i]
-    r[0]=_sum
-    r[1] = i
+c_a = dict()
+# c_b = dict()
+flag = False
+a = [3231 ,53858 ,19 ,5 ,59 ,498 ,520 ,7 ,9 ,10 ,11 ,12]
+b = [520 ,7 ,498 ,59 ,10 ,6 ,12]
 
-print(r)
+for i in a:
+    if i in c_a:
+        c_a[i]+=1
+    else:
+        c_a[i]=1
 
+for j in b:
+    if j in c_a:
+        c_a[j]+=1
+
+for k in b:
+    if c_a[k]==2:
+        flag = True
+    else:
+        flag = False
+        print("No")
+
+
+
+
+print(c_a)    
+
+
+
+
+if flag:
+    print("yes")
+else:
+    print("no")
